c869314db038760a2bfc2b35d8885b3c124d0839
Lucas Farias <lucasfarias14@outlook.com>
2020-03-28 13:12:59 -0300

    primeiro commit teste jenkins

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mensagem());
+            
+        }
+
+        public static string Mensagem()
+        {
+            return "Oi jenkins";
+        }
+    }
+}
---
+++ Teste/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld;
+
+namespace Teste
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Oii Jenkins", Program.Mensagem());
+        }
+    }
+}
